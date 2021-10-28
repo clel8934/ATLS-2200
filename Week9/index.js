d047ad142eaf011dfeac7f905ed0b2cf88f0fc09
clel8934 <89863899+clel8934@users.noreply.github.com>
2021-10-28 13:13:52 -0600

    up

---
+++ Week9/index.js
@@ -0,0 +1,16 @@
+let meals = ["lunch","dinner","brunch"];
+let images = ["dinner.jpg", "lunch.jpg", "brunch.jpg"];
+
+for(i=0; i<document.getElementByClassName("meal").length; i++){
+  console.log(meals[i]);
+  document.getElementsByClassName("meal")[i].innerHTML = "<h2>" + meals[i]; + "</h2>";
+  document.getElementByClassName("meal")[i].addEventListener('click', setActive, false);
+  document.getElementsByClassName("meal")[i].id = meals[i];
+}
+
+
+function setActive(e){
+  console.log('clicked');
+  e.target.classList.toggle('active');
+  console.log(e.target.innerHTML);
+}
