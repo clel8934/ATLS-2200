16f9ff7ea44d365b7c3bbe22ae4a7f0548b1b4b5
clel8934 <89863899+clel8934@users.noreply.github.com>
2021-10-29 19:04:04 -0600

    Summary

--- Midterm/index.js
+++ Midterm/index.js
@@ -1,5 +1,57 @@
-let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-// let images = ["lunch.jpg", "dinner.jpg", "brunch.jpg"];
+// let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+// // let images = ["lunch.jpg", "dinner.jpg", "brunch.jpg"];
+// let prevday;
+//
+// for (i=0; i<document.getElementsByClassName("day").length; i++){
+//   document.getElementsByClassName("day")[i].innerHTML = "<h2>" + days[i] + "</h2>";
+//
+//   document.getElementsByClassName("day")[i].addEventListener('click', setActive, false);
+//
+//   document.getElementsByClassName("day")[i].id = days[i];
+//
+//   // document.getElementById("one").addEventListener('click', setActiveButton, false);
+//   // document.getElementById("two").addEventListener('click', setActiveButton, false);
+//
+// }
+//
+// document.getElementById('monday').classList.toggle('active');
+// // document.getElementById("image").innerHTML = "<img src= 'assets/" + images[0] + "' >";
+//
+// function setActive(e){
+//
+//
+//   prevday = document.getElementsByClassName("active")[0].id;
+//   if(prevday != undefined){
+//     document.getElementById(prevday).classList.toggle("active");
+//     document.getElementById(prevday +"menu").style.display = "none";
+//
+//   }
+//   // console.log("previous meal: " + prevmeal);
+//   // prevmeal.classList.toggle('active');
+//
+//
+//   if(e.target.tagName == "H2"){
+//     e.target.parentNode.classList.toggle("active");
+//   }else{
+//     e.target.classList.toggle("active");
+//   }
+//
+//   day = document.getElementsByClassName('active')[0].id;
+//   console.log(day);
+//   document.getElementById(day +"menu").style.display = "block";
+//   // document.getElementById('image').innerHTML = "<img src='assets/" + meal + ".jpg' >";
+//
+// }
+//
+//
+// // function setActiveButton(e){
+// //   e.target.classList.toggle("active");
+// // }
+
+
+
+let days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+// let images = ["day1.jpeg", "day2.jpeg"];
 let prevday;
 
 for (i=0; i<document.getElementsByClassName("day").length; i++){
@@ -8,27 +60,23 @@ for (i=0; i<document.getElementsByClassName("day").length; i++){
   document.getElementsByClassName("day")[i].addEventListener('click', setActive, false);
 
   document.getElementsByClassName("day")[i].id = days[i];
-
-  // document.getElementById("one").addEventListener('click', setActiveButton, false);
-  // document.getElementById("two").addEventListener('click', setActiveButton, false);
-
 }
 
+
 document.getElementById('monday').classList.toggle('active');
-// document.getElementById("image").innerHTML = "<img src= 'assets/" + images[0] + "' >";
+// document.getElementById("image").innerHTML = "<img src=assets/" + images[0] + "' >";
 
 function setActive(e){
 
-
-  prevday = document.getElementsByClassName("active")[0].id;
-  if(prevday != undefined){
+prevday = document.getElementsByClassName("active")[0].id;
+if(prevday != undefined){
     document.getElementById(prevday).classList.toggle("active");
     document.getElementById(prevday +"menu").style.display = "none";
 
-  }
-  // console.log("previous meal: " + prevmeal);
-  // prevmeal.classList.toggle('active');
+}
 
+  // console.log("previous menu: " + prevmenu);
+  // prevmenu.classList.toggle('active');
 
   if(e.target.tagName == "H2"){
     e.target.parentNode.classList.toggle("active");
@@ -36,14 +84,9 @@ function setActive(e){
     e.target.classList.toggle("active");
   }
 
-  day = document.getElementsByClassName('active')[0].id;
-  console.log(day);
-  document.getElementById(day +"menu").style.display = "block";
-  // document.getElementById('image').innerHTML = "<img src='assets/" + meal + ".jpg' >";
+day = document.getElementsByClassName('active')[0].id;
+ console.log(day);
+ document.getElementById(day +"menu").style.display = "block";
+ // document.getElementById('image').innerHTML = "<img src='assets/" + day + ".jpeg' >";
 
 }
-
-
-// function setActiveButton(e){
-//   e.target.classList.toggle("active");
-// }
